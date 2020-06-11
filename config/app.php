0c9c912ddf63ef86dc9e23fd5d94ca0aabef628e
gid-dy <55790266+gid-dy@users.noreply.github.com>
2020-06-11 01:45:06 +0000

    app.php

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        'Webpatser\Countries\CountriesServiceProvider',
 
 
 
@@ -231,7 +230,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'Countries' => 'Webpatser\Countries\CountriesFacade',
 
     ],
 
